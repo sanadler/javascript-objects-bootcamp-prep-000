4289364b6ceb61bdbf643157ff36fc825fc548f9
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-08 02:20:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
-//var playlist = {artistName: 'songTitle'};
+var playlist = {};
+playlist.artistName: 'songTitle'
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle;
